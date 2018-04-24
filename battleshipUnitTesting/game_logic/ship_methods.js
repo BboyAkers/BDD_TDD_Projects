d518b9fc5212e7ab92258168bf3cb6f45a5affe9
Austin Akers <austin.akers5@gmail.com>
2018-04-23 23:41:08 -0500

    added logic for test case

--- battleshipUnitTesting/game_logic/ship_methods.js
+++ battleshipUnitTesting/game_logic/ship_methods.js
@@ -1,3 +1,19 @@
-function checkForShip() {}
+function checkForShip(player, coordinates) {
+  let shipPresent, ship;
+
+  for (let i = 0 < player.ships.length; i++; ) {
+    ship = player.ship[i];
+
+    shipPresent = ship.locations.filter(function(actualCoordinate) {
+      return (
+        actualCoordinate[0] === coordinates[0] &&
+        actualCoordinate[1] === coordinates[1]
+      );
+    })[0];
+    if (!shipPresent) {
+      return false;
+    }
+  }
+}
 
 module.exports.checkForShip = checkForShip;
--- battleshipUnitTesting/test/ship_test.js
+++ battleshipUnitTesting/test/ship_test.js
@@ -3,5 +3,14 @@ var expect = require("chai").expect;
 describe("checkForShip", function() {
   var checkForShip = require("../game_logic/ship_methods").checkForShip;
 
-  it("should correctly report no ship at a given players coordinate", function() {});
+  it("should correctly report no ship at a given players coordinate", function() {
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
 });
